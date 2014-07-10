d4c4d425303098befbad7ec18cb0a151254497e1
jkush321 <jkush321@yahoo.com>
2014-07-10 00:15:35 -0400

    Bool + placeholder types

---
+++ src/icac/impervium/server/datatypes/UInt64.java
@@ -0,0 +1,11 @@
+package icac.impervium.server.datatypes;
+
+public class UInt64 implements DataType {
+
+	@Override
+	public byte[] getBytes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/icac/impervium/server/datatypes/VLQString.java
@@ -0,0 +1,13 @@
+package icac.impervium.server.datatypes;
+
+public class VLQString implements DataType {
+	public VLQString(byte[] data)
+	{
+		
+	}
+
+	public byte[] getBytes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
